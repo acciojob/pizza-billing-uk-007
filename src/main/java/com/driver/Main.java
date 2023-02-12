343fe84afa35cecaa02f7f386e90a0c360261379
uk-007 <yadavutkarsh07@gmail.com>
2023-02-12 22:59:08 +0530

    commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     Pizza p = new Pizza(false);
 
-    p.addExtraToppings();
+    p.addExtraToppings()
     //p.addTakeaway();
     p.addExtraCheese();
     System.out.println(p.getBill());
