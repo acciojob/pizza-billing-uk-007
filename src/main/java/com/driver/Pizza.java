d49aa7706384a7afb4faef30f5ad079ac149def4
uk-007 <yadavutkarsh07@gmail.com>
2023-02-12 22:31:08 +0530

    another change

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -136,7 +136,7 @@ public class Pizza {
             if(isTakeAway){
                 this.bill += "Paperbag Added: 20"+"\n";
             }
-            this.bill += "Total Price: "+this.price+"\n";
+            this.bill += "Total Price: "+this.price;
             this.isBillCreated = true;
             return this.bill;
         }
